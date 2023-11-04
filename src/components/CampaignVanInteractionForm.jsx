b96989699655bcca05c8921c8ea5d7be4aa7f5be
RahulR100 <rahulrao@email.com>
2023-11-04 02:25:23 +0000

    update checklist and form component schema

--- src/components/CampaignVanInteractionForm.jsx
+++ src/components/CampaignVanInteractionForm.jsx
@@ -1,12 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
 import Form from "react-formal";
-import moment from "moment";
 import CampaignFormSectionHeading from "./CampaignFormSectionHeading";
 import GSForm from "./forms/GSForm";
-import GSColorPicker from "./forms/GSColorPicker";
-import GSTextField from "./forms/GSTextField";
-import GSDateField from "./forms/GSDateField";
 import GSSubmitButton from "./forms/GSSubmitButton";
 import * as yup from "yup";
 import { dataTest } from "../lib/attributes";
@@ -14,54 +10,22 @@ import { Autocomplete } from "@material-ui/lab";
 import { TextField } from "@material-ui/core";
 
 // TODO
-// Update schema
-// Write autocomplete function
-// Pull VAN API in
+// DONE: Update schema
+// DONE: Write autocomplete function (implemented using MUI component)
+// Pull VAN API in (fetch campaigns from server)
 // Make it only appear when VAN API keys are set
 
 const FormSchemaBeforeStarted = {
-  title: yup.string().required(),
-  description: yup
+  vanCampaignId: yup
     .string()
     .optional()
-    .default("No description provided"),
-  dueBy: yup
-    .mixed()
-    .required()
-    .test(
-      "in-future",
-      "Due date should be in the future: when you expect the campaign to end",
-      val => new Date(val) > new Date()
-    ),
-  logoImageUrl: yup
-    .string()
-    .url()
-    .transform(value => (!value ? null : value))
-    .nullable(),
-  primaryColor: yup.string().nullable(),
-  introHtml: yup.string().nullable()
+    .nullable()
 };
 
 const FormSchemaAfterStarted = {
-  title: yup.string().required(),
-  description: yup
+  vanCampaignId: yup
     .string()
     .optional()
-    .default("No description provided"),
-  dueBy: yup.mixed().required(),
-  logoImageUrl: yup
-    .string()
-    .transform(value => (!value ? null : value))
-    .url()
-    .nullable(),
-  primaryColor: yup
-    .string()
-    .matches(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/)
-    .transform(value => (!value ? null : value))
-    .nullable(),
-  introHtml: yup
-    .string()
-    .transform(value => (!value ? null : value))
     .nullable()
 };
 
@@ -112,16 +76,10 @@ const FormSchemaAfterStarted = {
 //   }
 
 export default class CampaignVanInteractionForm extends React.Component {
-  formSchema() {
-    if (this.props.ensureComplete) {
-      // i.e. campaign.isStarted
-      return yup.object(FormSchemaAfterStarted);
-    }
-    return yup.object(FormSchemaBeforeStarted);
-  }
-
   render() {
-    const formSchema = this.formSchema();
+    const formSchema = this.props.ensureComplete
+      ? yup.object(FormSchemaAfterStarted)
+      : yup.object(FormSchemaBeforeStarted);
 
     return (
       <div>
@@ -133,51 +91,8 @@ export default class CampaignVanInteractionForm extends React.Component {
           onSubmit={this.props.onSubmit}
           {...dataTest("campaignBasicsForm")}
         >
-          {/* <Form.Field
-            as={GSTextField}
-            {...dataTest("title")}
-            name="title"
-            label="Title (required)"
-            helpertext="e.g. Election Day 2016"
-            fullWidth
-          />
-          <Form.Field
-            as={GSTextField}
-            {...dataTest("description")}
-            name="description"
-            label="Description"
-            helpertext="Get out the vote"
-            fullWidth
-          />
-          <Form.Field
-            as={GSDateField}
-            {...dataTest("dueBy")}
-            name="dueBy"
-            label="Due date (required)"
-            locale="en-US"
-            shouldDisableDate={date => moment(date).diff(moment()) < 0}
-            fullWidth
-          />
-          <Form.Field
-            as={GSTextField}
-            name="introHtml"
-            label="Intro HTML"
-            multiline
-            fullWidth
-          />
-          <Form.Field
-            as={GSTextField}
-            name="logoImageUrl"
-            label="Logo Image URL"
-            helpertext="https://www.mysite.com/images/logo.png"
-            fullWidth
-          />
-          <Form.Field
-            as={GSColorPicker}
-            name="primaryColor"
-            label="Primary color"
-          /> */}
           <Autocomplete
+            name="vanCampaignId"
             renderInput={params => {
               return <TextField {...params} label="Choose a campaign below" />;
             }}
@@ -199,12 +114,7 @@ export default class CampaignVanInteractionForm extends React.Component {
 
 CampaignVanInteractionForm.propTypes = {
   formValues: PropTypes.shape({
-    title: PropTypes.string,
-    description: PropTypes.string,
-    dueBy: PropTypes.any,
-    logoImageUrl: PropTypes.string,
-    primaryColor: PropTypes.string,
-    introHtml: PropTypes.string
+    vanCampaignId: PropTypes.string
   }),
   onChange: PropTypes.func,
   onSubmit: PropTypes.func,
